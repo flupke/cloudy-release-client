6c3538758cc1e6350c149f9c350d463dfc24a138
Luper Rouch <luper.rouch@gmail.com>
2013-11-29 16:18:38 +0100

    added tests for find_deployment_variables() with JSON and Python formats

--- cloudyclient/tests/test_api.py
+++ cloudyclient/tests/test_api.py
@@ -11,7 +11,18 @@ DATA_DIR = op.join(op.dirname(__file__), 'data')
 def test_find_deployment_variables():
     location = DATA_DIR
     assert_equal(find_deployment_variables(location), None)
+    # Variables in YAML format
     location = op.join(DATA_DIR, 'checkout_sample', '.project.0')
     assert_equal(find_deployment_variables(location), {'a': 1, 'b': 3})
     location = op.join(DATA_DIR, 'checkout_sample', '.project.0', 'subdir')
+    assert_equal(find_deployment_variables(location), {'a': 1, 'b': 3})
+    # Variables in JSON format
+    location = op.join(DATA_DIR, 'checkout_sample', '.project-json.0')
+    assert_equal(find_deployment_variables(location), {'a': 1, 'b': 3})
+    location = op.join(DATA_DIR, 'checkout_sample', '.project-json.0', 'subdir')
+    assert_equal(find_deployment_variables(location), {'a': 1, 'b': 3})
+    # Variables in Python format
+    location = op.join(DATA_DIR, 'checkout_sample', '.project-python.0')
+    assert_equal(find_deployment_variables(location), {'a': 1, 'b': 3})
+    location = op.join(DATA_DIR, 'checkout_sample', '.project-python.0', 'subdir')
     assert_equal(find_deployment_variables(location), {'a': 1, 'b': 3})
